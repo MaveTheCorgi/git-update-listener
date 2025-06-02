2b21ba7dfa73fe479e3de2c7f71a149d158bb796
MaveTheCorgi <helladaemon@gmail.com>
2025-06-02 19:03:08 +0200

    Changed socket to listen on all network interfaces

--- src/main/java/dev/mave/gitupdatelistener/plugin/TriggerService.java
+++ src/main/java/dev/mave/gitupdatelistener/plugin/TriggerService.java
@@ -14,6 +14,7 @@ import dev.mave.gitupdatelistener.plugin.model.TriggerSettings;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -46,7 +47,7 @@ public final class TriggerService {
     private void runServer() {
         TriggerSettings settings = TriggerSettings.getInstance();
         try {
-            server = new ServerSocket(settings.listenPort);
+            server = new ServerSocket(settings.listenPort, 0, InetAddress.getByName("0.0.0.0"));
             while (isRunning.get() && !Thread.currentThread().isInterrupted()) {
                 try (Socket client = server.accept()) {
                     processWebhookRequest(client);
